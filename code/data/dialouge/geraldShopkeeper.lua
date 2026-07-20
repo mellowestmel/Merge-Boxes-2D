d89b9f05d42ab745d5664737de0c34bdab55659c
shiba <shibathedeveloper+github@gmail.com>
2026-07-20 17:00:56 +0200

    aa

---
+++ code/data/dialouge/geraldShopkeeper.lua
@@ -0,0 +1,19 @@
+return {
+    Greetings = {
+        {
+            dialouge = "Hi! Welcome to my shop!",
+            sprite = "/assets/sprites/portraits/gerald/happy.png"
+        }
+	},
+
+    BuyItem = {
+
+	},
+    OutOfStock = {
+
+	},
+	TooPoor = {
+
+	},
+
+}
